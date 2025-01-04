6e185deb2af66b334d9927360d66fb67c7dfecd8
CrazyBeastable <shrivedpatade@gmail.com>
2025-01-04 13:17:43 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ChatBot_with_Gemini_and_Langchain
+ Chat bot implementation with Gemini API and Langchain
---
+++ main.py
@@ -0,0 +1,15 @@
+import google.generativeai as genai
+
+genai.configure(api_key="AIzaSyBOHVCtrZQNJVncQwTTJJwTrWHHK38RGoM")
+confirmer = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-1.5-flash")
+
+def chatMedical(prompt):
+    if int(confirmer.generate_content("Return '1' if the question after ',' is medical field related else '0' , "+prompt).text):
+        response = model.generate_content(prompt).text
+    else:
+        response = "Your response doesn't seem to be Medical Field related. \nI am a Medical Field based Chat Bot, ask me anything related to it 😁."
+    return response
+        
+    
+print(chatMedical("how are you"))
